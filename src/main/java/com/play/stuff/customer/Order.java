2a1e41ca44c0e939ae76f564af1c0537c709e687
Cristi <cristi_zmf@yahoo.com>
2019-12-13 11:44:23 +0200

    increase value size of order

--- src/main/java/com/play/stuff/customer/Order.java
+++ src/main/java/com/play/stuff/customer/Order.java
@@ -4,7 +4,13 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.*;
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+import java.lang.management.MemoryUsage;
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Entity
 @Table(name = "c_order")
@@ -28,5 +34,17 @@ public class Order {
 
     public void setCustomer(Customer customer) {
         this.customer = customer;
+    }
+
+    public static void main(String[] args) {
+        MemoryMXBean mbean = ManagementFactory.getMemoryMXBean();
+        MemoryUsage beforeHeapMemoryUsage = mbean.getHeapMemoryUsage();
+        System.out.printf("Total memory used: %s\n", mbean.getHeapMemoryUsage());
+        ArrayList<Object> objects = new ArrayList<>();
+        System.out.printf("Total memory used after empty array creation: %s\n", mbean.getHeapMemoryUsage());
+        objects = null;
+        System.out.printf("Total memory used after deleting array: %s\n", mbean.getHeapMemoryUsage());
+        List<Object> emptyList = Collections.emptyList();
+        System.out.printf("Total memory used after creating empty list with emptyList method: %s\n", mbean.getHeapMemoryUsage());
     }
 }
---
+++ src/main/resources/db/migration/V1/V1_3_0__increase_value_column_of_order.sql
@@ -0,0 +1,3 @@
+alter table c_order
+    alter column value type varchar(500);
+
