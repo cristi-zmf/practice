a9190805b44390a29e673b9443790dbfd2e31bc9
Cristi <cristi_zmf@yahoo.com>
2019-12-13 03:24:45 +0200

    clean up of scripts

--- src/main/resources/db/migration/V1/V1_0_0__first.sql
+++ src/main/resources/db/migration/V1/V1_0_0__first.sql
@@ -1,4 +1,4 @@
-create sequence "customer_seq" start 1 increment 1;
+create sequence customer_seq start 1 increment 1;
 create table customer (
     id numeric (20) primary key,
     name varchar (60)
